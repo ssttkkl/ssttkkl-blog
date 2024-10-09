a47006d6ab9627f88c1cd63aced47efb45fc15ca
huangwenlong06 <huangwenlong06@meituan.com>
2024-10-09 18:56:20 +0800

    update

--- scripts/process_pending_doc.sh
+++ scripts/process_pending_doc.sh
@@ -13,12 +13,6 @@ doc_files=$(find "$directory" -type f -name "*.doc")
 # 使用 for 循环遍历所有找到的文件
 for file in $doc_files; do
     echo "Processing file: $file"
-    hexo_output=$(hexo new "$title" -p /$year/$month/$day/$title/$title.md)
-
-    # 输出示例：INFO  Created: ~/my-hexo-site/source/_posts/my-new-post.md
-    # 从命令输出中解析创建的文件路径
-    # 这里假设输出中的路径位于最后一段，并且输出包含 "Created: " 前缀
-    created_file=$(echo "$hexo_output" | grep -oP 'Created: \K(.*)')
 
     groovy -cp $CLASSPATH convert_doc.groovy $file source/_posts/$year/$month/$day/$title
 
